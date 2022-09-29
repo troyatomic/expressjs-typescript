5b76f4d8ec4f9c0f26dd4ddc90f0ef396a4ecad3
Troy Dugger <troy@atomicrobot.com>
2022-09-28 21:03:48 -0500

    a little bit working

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-
+import axios, { AxiosRequestConfig } from 'axios';
 
 export const register = (app: express.Application) => {
     // home page
@@ -7,8 +7,26 @@ export const register = (app: express.Application) => {
         res.render("index");
     });
 
-    // about page
-    app.get("/about", (req: any, res) => {
-        res.render("about");
+    app.get("/health", (req: any, res) => {
+        res.status(200).json({'status': 'OK'});
     });
+
+    // about page
+    app.get("/about", (_req: any, res) => {
+        const url = `${process.env.GITHUB_URL}/repos/${process.env.GITHUB_REPO}/issues`
+        const options:AxiosRequestConfig = {
+            method: 'GET',
+            headers: {
+                'accept': 'application/vnd.github+json',
+                'authorization': `Bearer ${process.env.GITHUB_TOKEN}`
+            },
+            url,
+        }
+        void axios(options).then((response) => {
+            // eslint-disable-next-line no-console
+            console.log('response', response.data);
+            res.status(200).render('about');
+        });
+    });
+
 };
