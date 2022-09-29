a91604ffb9926308d33a7fe9fdd1291f3837fcdb
Troy Dugger <troy@atomicrobot.com>
2022-09-28 22:33:19 -0500

    3 down 2 to go

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,25 +1,34 @@
 import * as express from "express";
-import axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig, AxiosRequestHeaders } from 'axios';
+
+type Issue = {
+    title: string,
+    id: number,
+    body: string | null
+}
+
 
 export const register = (app: express.Application) => {
+    const githubHeaders: AxiosRequestHeaders = {
+        'accept': 'application/vnd.github+json',
+        'authorization': `Bearer ${process.env.GITHUB_TOKEN}`
+    }
+
     // home page
-    app.get("/", (req: any, res) => {
+    app.get("/", (req, res) => {
         res.render("index");
     });
 
-    app.get("/health", (req: any, res) => {
+    app.get("/health", (req, res) => {
         res.status(200).json({'status': 'OK'});
     });
 
     // about page
-    app.get("/about", (_req: any, res) => {
+    app.get("/about", (req, res) => {
         const url = `${process.env.GITHUB_URL}/repos/${process.env.GITHUB_REPO}/issues`
         const options:AxiosRequestConfig = {
             method: 'GET',
-            headers: {
-                'accept': 'application/vnd.github+json',
-                'authorization': `Bearer ${process.env.GITHUB_TOKEN}`
-            },
+            headers: githubHeaders,
             url,
         }
         void axios(options).then((response) => {
@@ -29,4 +38,36 @@ export const register = (app: express.Application) => {
         });
     });
 
+    app.get("/api/v1/github/:owner/:repo/issue/:issue_number", (req, res) => {
+        const url = `${process.env.GITHUB_URL}/repos/${req.params.owner}/${req.params.repo}/issues/${req.params.issue_number}`;
+        const options:AxiosRequestConfig = {
+            method: 'GET',
+            headers: githubHeaders,
+            url,
+        }
+        void axios(options).then((response) => {
+            const issue: Issue = response.data as Issue;
+            // eslint-disable-next-line no-console
+            console.log('response', issue.title);
+            res.status(200).json({'id': issue.id, 'title': issue.title, 'body': issue.body});
+        });
+    });
+
+
+    app.get("/api/v1/github/:owner/:repo/issue/:issue_number/image", (req, res) => {
+        const url = `${process.env.GITHUB_URL}/repos/${req.params.owner}/${req.params.repo}/issues/${req.params.issue_number}`;
+        const options:AxiosRequestConfig = {
+            method: 'GET',
+            headers: githubHeaders,
+            url,
+        }
+        void axios(options).then((response) => {
+            const issue: Issue = response.data as Issue;
+            let hasImage = false;
+            if (issue.body?.includes('<img')) {
+                hasImage = true
+            }
+            res.status(200).json({'containsImage': hasImage});
+        });
+    });
 };
